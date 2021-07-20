498a23f47e0164daa5541694558ef01110fa0db3
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2021-07-20 15:27:08 +0300

    add save to localStorage

--- src/App.js
+++ src/App.js
@@ -7,15 +7,22 @@ import styles from './App.module.css';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      { id: 'id-5', name: 'Sarah Connor', number: '725-61-18' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const storageContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (storageContacts) {
+      this.setState({ contacts: storageContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = contact => {
     const { contacts } = this.state;
